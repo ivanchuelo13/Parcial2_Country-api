7b094f959dd57be835aacd62c8af25252e3d510f
Salas Didacticas Sede Bogota <salas@unilibre.edu.co>
2019-10-16 21:02:19 -0500

    Falta hacerlo con h2, pero funciona

--- src/main/java/co/edu/unilibre/rest/controller/CountryController.java
+++ src/main/java/co/edu/unilibre/rest/controller/CountryController.java
@@ -1,12 +1,9 @@
 package co.edu.unilibre.rest.controller;
 
-import java.awt.PageAttributes.MediaType;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,7 +15,7 @@ public class CountryController {
 	
 	@RequestMapping(value = "/contries", method =  RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE )
 	
-	public List<IContryService> allContrys(){
+	public List<IContryService> allContry(){
 		List<IContryService> country = new ArrayList<>();
 		IContryService country1 = new IContryService(1,"Polonia");
 		IContryService country2 = new IContryService(2,"United States");
@@ -27,5 +24,7 @@ public class CountryController {
 		country.add(country1);
 		country.add(country2);
 		country.add(country3);
+		
+		return country;
 	}
 }
